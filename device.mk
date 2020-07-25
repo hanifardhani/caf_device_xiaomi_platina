7c0503bafadb224240b29bdbd87aebc62900ac3e
Hanif Ardhani <hanif.ardhani98@gmail.com>
2020-07-25 02:51:07 +0000

    platina: Nuke telephony-ext boot jars

--- device.mk
+++ device.mk
@@ -445,10 +445,6 @@ PRODUCT_PACKAGES += \
     rild \
     telephony-ext
 
-# RIL JAR
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
